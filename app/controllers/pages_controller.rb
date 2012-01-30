f10472fe622e7eebc0d7e2e419bd66a248628dbd
Elyse Dougherty <elysedougherty@gmail.com>
2012-01-29 22:52:30 -0500

    Cleaned up extra files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,7 @@ class PagesController < ApplicationController
   def signup
     @potential = Potential.new(params[:potential])
     if @potential.save
+
       redirect_to(root_path, :notice => 'Email successfully submitted. Thanks!')
     else
       render 'home'
--- db/migrate/20120121193228_change_payment_method_column_to_type_column.rb
+++
@@ -1,9 +0,0 @@
-class ChangePaymentMethodColumnToTypeColumn < ActiveRecord::Migration
-  def self.up
-    add_column :payments, :payment_type, :string
-  end
-
-  def self.down
-    remove_column :payments, :payment_type
-  end
-end
